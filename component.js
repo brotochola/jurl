6b782c3182d1d877cf88e843420a216c442f4737
saiegh@gmail.com <saiegh@gmail.com>
2023-09-05 14:51:08 -0300

    {{ }}

--- component.js
+++ component.js
@@ -1,6 +1,6 @@
 class Component extends HTMLElement {
   constructor() {
-    console.log("constructor Component!");
+    // console.log("constructor Component!");
     super();
 
     this.uid = Math.floor(Math.random() * 999999999);
@@ -9,7 +9,7 @@ class Component extends HTMLElement {
 
     this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = app.components[nameOfClassThatCalledThis].html;
-
+    this.shadowRoot.componentReference = this;
     //STYLE:
     this.shadowRoot.append(
       app.components[nameOfClassThatCalledThis].css.cloneNode(true)
@@ -19,19 +19,61 @@ class Component extends HTMLElement {
 
     app.instanciatedComponents.push(this);
     this.setRootElement();
+    this.initObsrever();
     this.updateParsedAttributes();
     // console.log(Array.from(this.attributes));
-    this.initObsrever();
+  }
+
+  getParentComponent() {
+    let obj = this;
+    while (obj.parentNode) {
+      obj = obj.parentNode;
+    }
+    return obj.componentReference;
   }
   setRootElement() {
     this.root = this.$(this.constructor.name.toLowerCase());
   }
   updateParsedAttributes() {
     this.parsedAttributes = {};
+
+    this.observer.disconnect();
+
     Array.from(this.attributes).map((k) => {
-      this.parsedAttributes[k.name] = k.value;
-      this.root.setAttribute(k.name, k.value);
+      if (k.value.endsWith("}}") && k.value.startsWith("{{")) {
+        //CURLY BRACKETS VARIABLES
+        let val = k.value.substr(2, k.value.length - 4);
+
+        try {
+          this.parsedAttributes[k.name] =
+            this.getParentComponent().getAttribute(val);
+        } catch (e) {
+          // console.warn(e);
+        }
+      } else {
+        this.parsedAttributes[k.name] = k.value;
+        // this.root.setAttribute(k.name, k.value);
+        this.style.setProperty("--" + k.name, k.value);
+      }
     });
+
+    this.observer.observe(this, this.observerConfig);
+  }
+
+  getAllChildrenComponents() {
+    return Array.from(this.root.querySelectorAll("*")).filter((k) =>
+      k.tagName.toLowerCase().startsWith("app-")
+    );
+  }
+
+  update() {
+    this.updateParsedAttributes();
+    if (this.onChange instanceof Function) {
+      this.onChange(this.parsedAttributes);
+    }
+    for (let c of this.getAllChildrenComponents()) {
+      if (c.update instanceof Function) c.update();
+    }
   }
 
   handleOnChange(mutationList) {
@@ -40,32 +82,35 @@ class Component extends HTMLElement {
         // console.log("A child node has been added or removed.");
       } else if (mutation.type === "attributes") {
         // console.log(`The ${mutation.attributeName} attribute was modified.`);
-        this.updateParsedAttributes();
-        if (this.onChange instanceof Function) {
-          this.onChange(this.parsedAttributes);
-        }
+        this.update();
         // console.log(this.parsedAttributes);
       }
     }
   }
 
   initObsrever() {
-    const config = { attributes: true, childList: true, subtree: true };
+    this.observerConfig = {
+      attributes: true,
+      childList: false,
+      subtree: false,
+    };
 
     // Create an observer instance linked to the callback function
     this.observer = new MutationObserver((e) => this.handleOnChange(e));
 
     // Start observing the target node for configured mutations
-    this.observer.observe(this, config);
+    this.observer.observe(this, this.observerConfig);
 
     // Later, you can stop observing
     // observer.disconnect();
   }
   connectedCallback() {
-    console.log(this.constructor.name, this.uid, "appeared");
+    // console.log(this.constructor.name, this.uid, "appeared");
+    setTimeout(() => this.updateParsedAttributes(), 1);
+    if (this.onInit instanceof Function) this.onInit();
   }
   disconnectedCallback() {
-    console.log(this.constructor.name, this.uid, "died", this);
+    // console.log(this.constructor.name, this.uid, "died", this);
 
     this.observer.disconnect();
     app.instanciatedComponents = app.instanciatedComponents.filter(
@@ -76,11 +121,11 @@ class Component extends HTMLElement {
     console.log("attr changed", this.uid, attrName, oldVal, newVal);
   }
 
-  getElementInDOM() {
-    let elem = app.root.querySelector("*[uid='" + this.uid + "']");
-    elem.instanceReference = this;
-    return elem;
-  }
+  // getElementInDOM() {
+  //   let elem = app.root.querySelector("*[uid='" + this.uid + "']");
+  //   elem.instanceReference = this;
+  //   return elem;
+  // }
 
   $(sel) {
     let res = this.shadowRoot.querySelectorAll(sel);
--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+const randomColor = () => Math.floor(Math.random() * 16777215).toString(16);
